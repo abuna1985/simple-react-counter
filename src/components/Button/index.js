8a65c358704f2fb53951b995ca7d4594c897db5c
abuna1985 <adamjabundis@gmail.com>
2021-01-12 01:26:01 -0800

    Added styling and logic for reset button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 import styles from './Button.module.css'; 
 
 const Button = ({handleClick, children, type, testId, isDisabled }) => {
-  if (type === "zero") {
-    return <button data-testid={testId} onClick={handleClick} className={styles.zero} ><span>{children}</span></button>;
-  } 
-
-  return  <button data-testid={testId} onClick={handleClick} className={styles.counter} disabled={isDisabled}><span>{children}</span></button>;
+   return  <button data-testid={testId} onClick={handleClick} className={`${styles.btn} ${type === "reset" ? styles.reset : styles.counter}`} disabled={isDisabled}><span>{children}</span></button>;
 }
 
 export default Button;
