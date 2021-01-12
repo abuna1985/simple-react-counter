ed30b4f81ed54877c49fd57326ed6f9d927bd76b
abuna1985 <adamjabundis@gmail.com>
2021-01-12 00:01:32 -0800

    Updated Component logic and styling for Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './Button.module.css'; 
 
-const Button = ({handleClick, text, type }) => {
+const Button = ({handleClick, children, type, testId, isDisabled }) => {
   if (type === "zero") {
-    return <button onClick={handleClick} className={styles.zero}>{text}</button>;
+    return <button data-testid={testId} onClick={handleClick} className={styles.zero} ><span>{children}</span></button>;
   } 
 
-  return  <button onClick={handleClick} className={styles.counter}>{text}</button>;
+  return  <button data-testid={testId} onClick={handleClick} className={styles.counter} disabled={isDisabled}><span>{children}</span></button>;
 }
 
 export default Button;
